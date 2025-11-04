a077c150833e13e46bb19c859297c9cc1a178231
Gari <garinovoselskyyjaka@gmail.com>
2025-11-04 11:57:46 +0100

    Esqueleto del proyecto

---
+++ Tamagotchi/src/main/java/org/Cuatrovientos/Dam/Psp/Tamagotchi/App.java
@@ -0,0 +1,13 @@
+package org.Cuatrovientos.Dam.Psp.Tamagotchi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
